2b68008eb23b1458416602d34dd4ddae4e94092c
Jorge Vinicio Quintero Santos <a01634502@itesm.mx>
2020-11-02 04:58:31 -0700

    Added example socket with test client
    Signed-off-by: Jorge Vinicio Quintero Santos <a01634502@itesm.mx>

---
+++ backend/src/main/java/com/soldadosvini/FinalProject/messages/TestResponse.java
@@ -0,0 +1,22 @@
+package com.soldadosvini.FinalProject.messages;
+
+public class TestResponse {
+    public String content;
+
+    public TestResponse() {
+    }
+
+    public TestResponse(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    
+}
