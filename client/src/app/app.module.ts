2d8603da5833386bdf4ca6ff299bd5fcb20c67c1
Diego <diegoar7@live.com>
2020-11-03 22:00:00 -0600

    Adding modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
+import { ConcurrentSchemerComponent } from './concurrent-schemer/concurrent-schemer.component';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConcurrentSchemerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzGridModule,
+    NzSliderModule,
+    NzCheckboxModule,
+    NzButtonModule,
+    NzInputNumberModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
