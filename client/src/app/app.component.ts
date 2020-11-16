9ce542d4f256232c2e37228c6037b8fc4014cf2c
deficts <diegoar7@live.com>
2020-11-15 21:06:05 -0600

    Socket.io => Stomp & SockJS

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocketioService } from './services/socketio.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lang-project';
+  greeting: string;
+  socketIoService: SocketioService;
+
+  ngOnInit(){
+    this.socketIoService = new SocketioService(new AppComponent)
+  }
+
+  connect(){
+    this.socketIoService.connect();
+  }
+
+  disconnect(){
+    this.socketIoService.disconnect();
+  }
+
+  handleMessage(message){
+    this.greeting = message;
+  }
+
 }
+
--- client/src/app/services/socketio.service.ts
+++ client/src/app/services/socketio.service.ts
@@ -1,35 +1,50 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { io } from 'socket.io-client';
 import { environment } from 'src/environments/environment';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { AppComponent } from '../app.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SocketioService {
-  public socket;
+  stompClient: any;
+  topic: string = '/topic/greetings';
+  appComponent: AppComponent;
 
-  constructor() {
-    this.socket = io(environment.SOCKET_ENDPOINT)
+  constructor(appComponent: AppComponent) {
+    this.appComponent = appComponent;
   }
 
-  public sendSchemeOperation(schemeOperation: string, id: number) : void{
-    this.socket.emit('scheme-operation',schemeOperation,id)
-  }
-
-  public getProgress = () => {
-    return new Observable((observer)=>{
-      this.socket.on('progress-notification', (progressObj)=>{
-        observer.next(progressObj);
+  public connect(){
+    console.log('%c Initializing socket connection.','color: #22a322');
+    let webSocket = new SockJS(environment.WEB_SOCKET_ENDPOINT);
+    this.stompClient = Stomp.over(webSocket);
+    const _this = this;
+    _this.stompClient.connect({}, (frame)=>{
+      _this.stompClient.subscribe(_this.topic, (event)=>{
+        _this.onMessageRecieved(event);
       })
-    })
+    }, _this.errorCallback)
   }
 
-  public getResult = () => {
-    return new Observable((observer)=>{
-      this.socket.on('new-result', (resultObj)=>{
-        observer.next(resultObj);
-      })
-    })
+  public disconnect() {
+    if (this.stompClient !== null) {
+        this.stompClient.disconnect();
+    }
+    console.log("Disconnected");
+  }
+
+  public errorCallback(error) {
+    console.log("errorCallBack -> " + error)
+    setTimeout(() => {
+        this.connect();
+    }, 5000);
+  }
+
+  onMessageRecieved(message) {
+    console.log("Message Recieved from Server :: " + message);
+    this.appComponent.handleMessage(JSON.stringify(message.body));
   }
 }
