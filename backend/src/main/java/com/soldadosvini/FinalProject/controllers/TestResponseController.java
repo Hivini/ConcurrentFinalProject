c8c15ac0ff018b5e4eef3ae6511a18c7d12dbd30
Diego <diegoar7@live.com>
2020-11-19 17:20:28 -0600

    Creating observable in message controller

--- backend/src/main/java/com/soldadosvini/FinalProject/controllers/TestResponseController.java
+++ backend/src/main/java/com/soldadosvini/FinalProject/controllers/TestResponseController.java
@@ -3,15 +3,23 @@ package com.soldadosvini.FinalProject.controllers;
 import com.soldadosvini.FinalProject.messages.TestMessage;
 import com.soldadosvini.FinalProject.messages.TestResponse;
 
+import com.soldadosvini.FinalProject.producerConsumer.ProducerConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import java.util.Observable;
+
 @Controller
 public class TestResponseController {
+
+    @Autowired
+    public SimpMessageSendingOperations messagingTemplate;
 
     Logger logger = LoggerFactory.getLogger(TestResponseController.class);
 
@@ -19,8 +27,26 @@ public class TestResponseController {
     @SendTo("/topic/greetings")
     public TestResponse send(TestMessage message) throws Exception {
         // We can take as long as we want here to send back the message.
-        logger.info("This is an example log, it's useful.");
-        Thread.sleep(1000);
+        ProducerConsumer pc = new ProducerConsumer(2,2, 10,
+                new Character[]{'+','-'}, 1,9, 1000);
+
+        pc.start();
+
+        Observer myObserver = new Observer();
+        pc.buffer.addObserver(myObserver);
+
         return new TestResponse("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
+
+    class Observer implements java.util.Observer{
+
+        @Override
+        public void update(Observable o, Object arg) {
+            System.out.println("From obs: ");
+            System.out.println(arg);
+            messagingTemplate.convertAndSend("/topic/greetings",arg.toString());
+        }
+    }
 }
+
+
