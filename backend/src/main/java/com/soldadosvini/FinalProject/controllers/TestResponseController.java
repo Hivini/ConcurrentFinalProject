c9513a0cac71f55d1436412c4651cb130c13bd62
Fer Benavides <nockzblack@gmail.com>
2020-11-22 09:45:19 -0600

    writing method init

--- backend/src/main/java/com/soldadosvini/FinalProject/controllers/TestResponseController.java
+++ backend/src/main/java/com/soldadosvini/FinalProject/controllers/TestResponseController.java
@@ -13,7 +13,9 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import java.util.ArrayList;
 import java.util.Observable;
+import java.util.StringTokenizer;
 
 @Controller
 public class TestResponseController {
@@ -23,30 +25,65 @@ public class TestResponseController {
 
     Logger logger = LoggerFactory.getLogger(TestResponseController.class);
 
+    /*
+     * @MessageMapping("/hello")
+     * 
+     * @SendTo("/topic/greetings") public TestResponse send(TestMessage message)
+     * throws Exception { // We can take as long as we want here to send back the
+     * message. ProducerConsumer pc = new ProducerConsumer(2, 2, 10, new Character[]
+     * { '+', '-' }, 1, 9, 1000);
+     * 
+     * pc.start();
+     * 
+     * Observer myObserver = new Observer(); pc.buffer.addObserver(myObserver);
+     * 
+     * return new TestResponse("Hello, " + HtmlUtils.htmlEscape(message.getName()) +
+     * "!"); }
+     */
+
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public TestResponse send(TestMessage message) throws Exception {
-        // We can take as long as we want here to send back the message.
-        ProducerConsumer pc = new ProducerConsumer(2,2, 10,
-                new Character[]{'+','-'}, 1,9, 1000);
+    public TestResponse init(TestMessage message) throws Exception {
+        // Format: "Cons, Prods, Buffer Size, minRange, maxRange, wait time, Operator1,
+        // Operator2... Operatorn"
+        // Format: "2,2,10,1,9,500,+,-,/,*"
+
+        String parametersStr = HtmlUtils.htmlEscape(message.getName());
+
+        StringTokenizer auxTokenizer = new StringTokenizer(parametersStr, ",");
+        int nConsumers = Integer.parseInt(auxTokenizer.nextToken());
+        int nProducers = Integer.parseInt(auxTokenizer.nextToken());
+        int bufferSize = Integer.parseInt(auxTokenizer.nextToken());
+        int minRange = Integer.parseInt(auxTokenizer.nextToken());
+        ;
+        int maxRange = Integer.parseInt(auxTokenizer.nextToken());
+        ;
+        int waitRime = Integer.parseInt(auxTokenizer.nextToken());
+        ArrayList<Character> operatorsArrayList = new ArrayList<>();
+
+        while (auxTokenizer.hasMoreTokens()) {
+            operatorsArrayList.add(auxTokenizer.nextToken().charAt(0));
+        }
+
+        Character[] operatorsArray = new Character[operatorsArrayList.size()];
+        operatorsArrayList.toArray(operatorsArray);
+
+        ProducerConsumer pc = new ProducerConsumer(nConsumers, nProducers, bufferSize, operatorsArray, minRange,
+                maxRange, waitRime);
 
         pc.start();
 
-        Observer myObserver = new Observer();
-        pc.buffer.addObserver(myObserver);
+        return new TestResponse("Producer Consumer Started correctly");
 
-        return new TestResponse("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 
-    class Observer implements java.util.Observer{
+    class Observer implements java.util.Observer {
 
         @Override
         public void update(Observable o, Object arg) {
             System.out.println("From obs: ");
             System.out.println(arg);
-            messagingTemplate.convertAndSend("/topic/greetings",arg.toString());
+            messagingTemplate.convertAndSend("/topic/greetings", arg.toString());
         }
     }
 }
-
-
