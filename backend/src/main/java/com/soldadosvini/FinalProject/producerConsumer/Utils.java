bc7df7d835628592a0bcc321428aad64b3362ac5
Fer Benavides <nockzblack@gmail.com>
2020-11-22 12:41:13 -0600

    Removing unecesarry code on Utils.java
    Signed-off-by: Fer Benavides <nockzblack@gmail.com>

--- backend/src/main/java/com/soldadosvini/FinalProject/producerConsumer/Utils.java
+++ backend/src/main/java/com/soldadosvini/FinalProject/producerConsumer/Utils.java
@@ -4,8 +4,6 @@ import java.util.Random;
 
 public class Utils {
 
-    private static final int MAX_RANDOM = 100;
-    
     /* TODO(hivini): Call Scheme program instead. */
     public static String resolveTask(String task) {
         // Get all elements that are separated by a whitespace.
@@ -15,7 +13,8 @@ public class Utils {
         int current = 3;
         float result = Float.parseFloat(elements[2]);
         while (current < elements.length) {
-            if (elements[current].equals(")")) break;
+            if (elements[current].equals(")"))
+                break;
             switch (operation) {
                 case '+':
                     result += Float.parseFloat(elements[current++]);
@@ -36,40 +35,24 @@ public class Utils {
         }
         return String.format("%.2f", result);
     }
-    
+
     /**
      * Generates a new task that the @{Producer} can use.
      * 
-     * The task is an Scheme operation as a String.
-     * The following example is the sum of 1 + 2:
-     *  - "( + 1 2 )"
-     * Both values and operation are randomly generated.
+     * The task is an Scheme operation as a String. The following example is the sum
+     * of 1 + 2: - "( + 1 2 )" Both values and operation are randomly generated.
      * 
      * @return The Scheme operation as a String.
      */
     public static String generateTask(int min, int max, Character operator) {
         Random random = new Random();
 
-        int val1  = random.ints(min, max + 1).findFirst().getAsInt();
-        int val2  = random.ints(min, max + 1).findFirst().getAsInt();
+        int val1 = random.ints(min, max + 1).findFirst().getAsInt();
+        int val2 = random.ints(min, max + 1).findFirst().getAsInt();
 
         // Set a value of the operation as an int between min and max.
-        //String operator = getOperator(random.nextInt(4));
+        // String operator = getOperator(random.nextInt(4));
         return String.format("( %s %d %d )", operator, val1, val2);
     }
 
-    private static String getOperator(int val) {
-        switch(val) {
-            case 0:
-                return "+";
-            case 1:
-                return "-";
-            case 2:
-                return "*";
-            case 3:
-                return "/"; 
-            default:
-                return null;
-        }
-    }
 }
