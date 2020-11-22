bb2ed9a8f53e74e20651bb3b400b0ac25ab8f021
Fer Benavides <nockzblack@gmail.com>
2020-11-22 09:00:05 -0600

    removing imports on Producer Consumer
    Signed-off-by: Fer Benavides <nockzblack@gmail.com>

--- backend/src/main/java/com/soldadosvini/FinalProject/producerConsumer/ProducerConsumer.java
+++ backend/src/main/java/com/soldadosvini/FinalProject/producerConsumer/ProducerConsumer.java
@@ -1,15 +1,5 @@
 package com.soldadosvini.FinalProject.producerConsumer;
 
-
-import com.soldadosvini.FinalProject.producerConsumer.Buffer;
-import com.soldadosvini.FinalProject.producerConsumer.Consumer;
-import com.soldadosvini.FinalProject.producerConsumer.Producer;
-import com.soldadosvini.FinalProject.producerConsumer.Utils;
-
-import java.sql.Array;
-import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.ArrayList;
 
 public class ProducerConsumer {
@@ -22,23 +12,21 @@ public class ProducerConsumer {
     private int maxRange;
     private int waitTime;
 
-
-
-    public ProducerConsumer(int consumers, int producers, int bufferSize, Character[] operators, int minRange, int maxRange, int waitTime) {
+    public ProducerConsumer(int consumers, int producers, int bufferSize, Character[] operators, int minRange,
+            int maxRange, int waitTime) {
         this.buffer = new Buffer(bufferSize);
 
         this.operators = operators;
         this.maxRange = maxRange;
         this.minRange = minRange;
         this.waitTime = waitTime;
-       
+
         this.producers = new ArrayList<>();
         initProducers(producers);
 
         this.consumers = new ArrayList<>();
         initConsumers(consumers);
-        
-        
+
     }
 
     public void initConsumers(int size) {
@@ -55,14 +43,13 @@ public class ProducerConsumer {
 
     public void start() {
 
-        for (Producer  auxProducer : this.producers) {
+        for (Producer auxProducer : this.producers) {
             auxProducer.start();
         }
 
-        for (Consumer  auxConsumer : this.consumers) {
+        for (Consumer auxConsumer : this.consumers) {
             auxConsumer.start();
         }
     }
-
 
 }
